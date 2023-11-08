6e34f782fccf48ece287853c2fb355a61142ad39
vbf23 <149330539+vbf23@users.noreply.github.com>
2023-11-08 16:24:11 -0500

    Create install_oci.sh

---
+++ install_oci.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update the system
+sudo yum update -y
+
+# Install Python3 and pip3
+sudo yum install python3 -y
+sudo alternatives --set python /usr/bin/python3
+sudo yum install python3-pip -y
+
+# Install Flask
+pip3 install Flask
+
+# Install HTTP Server (Apache)
+sudo yum install httpd -y
+
+# Start and enable Apache
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+# Allow HTTP traffic in the firewall
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --reload
